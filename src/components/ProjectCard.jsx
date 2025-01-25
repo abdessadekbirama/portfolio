2d579a22b28cc9435a31feaf0422c0596e5c6786
abdessadek birama <abdessadekbirama1@gmail.com>
2025-01-25 18:47:40 +0100

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -29,7 +29,7 @@ function ProjectCard({img,title,description,source,demo}){
         whileInView={{opacity:100}}
         transition={{duration:1}}
          onMouseOver={Hover} onMouseOut={Blur} className={`p- pt-0 rounded-3xl flex flex-col gap-2 items-center size-[300px] shadow-[0_0_30px_1px_grey] overflow-hidden ${light&&"text-black"} `}>
-            <img className={`rounded w-[300px] transition-all duration-300 ${hover&&"scale-125 -z-30 brightness-50 "}`} src={`/imgs/${img}`} alt=""/>
+            <img className={`rounded w-[300px] max-h-[150px] transition-all duration-300 ${hover&&"scale-125 -z-30 brightness-50 "}`} src={`/imgs/${img}`} alt=""/>
             <div className={`flex gap-3 absolute mt-16 opacity-0 transition-all duration-300 ${hover&&"opacity-100"}`}>
                 <Icon onClick={visitSource} className="bg-gray-300 text-black size-10 rounded-full p-2 hover:scale-110 transition-all hover:cursor-pointer" icon={gh}/>
                 <button onClick={visitDemo} className="bg-gray-300 text-black size-10 rounded-full flex hover:scale-110 transition-all justify-center items-center"><Icon icon={web}/></button>
