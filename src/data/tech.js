d4496031fce49fbb99044c2579fd1f6168cabf00
abdessadek birama <abdessadekbirama1@gmail.com>
2025-01-25 17:51:33 +0100

    Delete src directory

--- src/data/tech.js
+++
@@ -1,7 +0,0 @@
-const technologies = [
-    "html.png","css.png","js.png",
-    "nodejs.png","react.png","python.png",
-    "tailwind.png","mysql.png","github.png",
-    "mongodb.png","php.png","chat-gpt.png"
-]
-export default technologies;
