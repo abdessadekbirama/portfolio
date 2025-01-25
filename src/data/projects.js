005af633fa869aab12272760a191a13302385acb
abdessadek birama <abdessadekbirama1@gmail.com>
2025-01-25 18:43:22 +0100

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,31 +1,25 @@
 const projects = [
     {
-        title:"News",
-        img:"blog.jpg",
-        description:"A News App delivering real-time updates and headlines.
-
-",
-        source:"https://github.com/abdessadekbirama/blog.git",
-        demo:"https://abdo-blog.vercel.app/"
+        title: "News",
+        img: "blog.jpg",
+        description: `A News App delivering real-time updates and headlines.`,
+        source: "https://github.com/abdessadekbirama/blog.git",
+        demo: "https://abdo-blog.vercel.app/"
     },
     {
-        title:"Weather app",
-        img:"weather.jpg",
-        description:"A Weather App providing real-time forecasts and conditions.
-
-",
-        source:"https://github.com/abdessadekbirama/weather-app.git",
-        demo:"https://abdo-weather.vercel.app/"
+        title: "Weather app",
+        img: "weather.jpg",
+        description: `A Weather App providing real-time forecasts and conditions.`,
+        source: "https://github.com/abdessadekbirama/weather-app.git",
+        demo: "https://abdo-weather.vercel.app/"
     },
     {
-        title:"Vidube",
-        img:"vidube.jpg",
-        description:"A Video App for streaming and watching content easily.
-
-",
-        source:"https://github.com/abdessadekbirama/vidube.git",
-        demo:"https://vidube.vercel.app/"
+        title: "Vidube",
+        img: "vidube.jpg",
+        description: `A Video App for streaming and watching content easily.`,
+        source: "https://github.com/abdessadekbirama/vidube.git",
+        demo: "https://vidube.vercel.app/"
     },
-    
-]
+];
+
 export default projects;
